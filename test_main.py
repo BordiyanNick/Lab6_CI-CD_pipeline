56043bbdf5023fd0fe567068443d51e84ef9bdde
BordiyanNick <162913412+BordiyanNick@users.noreply.github.com>
2026-06-11 00:40:43 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,9 +9,9 @@ class TestAPI(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    def test_status_code(self):
+    def test_calculate_route(self):
         response = self.app.get('/calculate')
-        self.assertNotEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
